078a86904782c221ffdc744b0b77c2a925f9ad3c
Lucas Dias Hudson <72749364+lucashudson2002@users.noreply.github.com>
2023-04-20 11:01:24 -0300

    tirando comentários

--- aula5/banco.h
+++ aula5/banco.h
@@ -3,18 +3,18 @@
 
 #include "conta.h"
 
-#define NUMCONTAS 100 //Define que a palavar NUMCONTAS passa a valer como escrever 100
+#define NUMCONTAS 100
 
 class Banco
 {
 private:
-    Conta contas[NUMCONTAS];//Cria um vetor de objetos do tipo Conta que pode armazenar até 100 contas
+    Conta contas[NUMCONTAS];
     int quantidade;
     int senhaFuncionario;
 public:
     Banco();
     ~Banco();
-    Conta* buscaConta(int numero); //Metodo que retorna o endereço do objeto conta que possui o mesmo numero informado
+    Conta* buscaConta(int numero);
     void atendimentoCliente();
     void atendimentoFuncionario();
     bool adicionaConta(int senha, int numero, std::string titular, std::string tipo, double saldo);
